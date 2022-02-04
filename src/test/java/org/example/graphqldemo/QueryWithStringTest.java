babbfdf2f694a445fa27bb3a5c1f9f5072edbfb0
Joe <jgliniecki@gmail.com>
2022-02-04 11:43:27 -0600

    Rename test

--- src/test/java/org/example/graphqldemo/QueryWithStringTest.java
+++ src/test/java/org/example/graphqldemo/QueryWithStringTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.example.graphqldemo.BaseTest.*;
 
-public class QueryTest {
+public class QueryWithStringTest {
 
   @Test
   void basicQueryWithTextBlock() {
